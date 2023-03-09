4bb9ede6411f7f9d0a0d97d6ad664ba98481f100
Matheus Fontes <matheusfontes1921@gmail.com>
2023-03-09 14:34:21 -0300

    criação

---
+++ src/components/ChatTeste.js
@@ -0,0 +1,11 @@
+import e_tudo_sobre_voce from '../img/e_tudo_sobre_voce.jpeg'
+import emaus from '../img/emaus.jpeg'
+import lembre_se_90 from '../img/lembre_se_90.jpeg'
+import capa from '../img/capa.jpg'
+
+
+import styles from './Home.module.css'
+
+function Home(){
+    return()
+}
